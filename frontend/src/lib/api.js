ae31b098e0b235c1e358bb37c8040cc0952d0945
shubham14mmn <shubham14mmn@gmail.com>
2026-05-21 20:48:08 +0530

    Fixed production API URL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+baseURL:
+import.meta.env.VITE_API_URL ||
+"https://chat-app-02lt.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+if (token) {
+config.headers.Authorization = `Bearer ${token}`;
+}
 
-  return config;
+return config;
 });
 
 export default api;
