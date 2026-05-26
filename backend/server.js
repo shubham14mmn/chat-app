837b2b99e51f91daa371fd5eb8ef45c8f4420414
shubham14mmn <shubham14mmn@gmail.com>
2026-05-26 13:13:43 +0530

    final cors fix

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://chat-app-iota-seven-71.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,8 +1,9 @@
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://chat-app-iota-seven-71.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
+
